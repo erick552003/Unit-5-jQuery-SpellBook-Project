c565a217181493c7ac79351acfd1346ce2ec6824
erick552003 <48761989+erick552003@users.noreply.github.com>
2019-03-20 10:10:58 -0400

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,24 @@
+//Spell #1
+
+$("goal").click(function(){
+ $("img").fadeToggle();
+});
+
+
+
+//Spell #2
+
+$("      ").click(function(){
+ 
+});
+
+
+
+//Spell #3
+
+$("      ").click(function(){
+ 
+});
+
+
+
